fa90f4db0ed4113d94fab52aeded2f74cd2afdc8
Mark Black <blackmarkt@gmail.com>
2022-10-10 17:21:58 -0700

    add navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,32 @@
 import Link from 'next/link'
+import Image from 'next/Image'
 import navStyles from '../styles/Nav.module.css'
+import Net0LabsLogo from '../public/net0labs/net0labs_header.png'
 
 const Nav = () => {
   return (
     <nav className={navStyles.nav}>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
-      </ul>
+        <Link href='/'>
+            <Image
+                src={Net0LabsLogo}
+                alt='Net0Labs'
+            />
+        </Link>
+        <div className={navStyles.headerRight}>
+            <Link href="/dashboard">
+                <button className={navStyles.buttonHead}>
+                    Dashboard
+                </button>
+            </Link>
+            <Link href="/partners">
+                <button className={navStyles.buttonHead}>
+                    Providers
+                </button>
+            </Link>
+            <div className={navStyles.btnLaunch}>
+            Launching Soon
+            </div>
+        </div>
     </nav>
   )
 }
