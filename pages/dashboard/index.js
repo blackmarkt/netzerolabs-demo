b7a9ed246fdf7f268a8595b370e13bcf3480e736
Mark Black <blackmarkt@gmail.com>
2022-10-11 18:07:08 -0700

    add network container & charts

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,12 +1,26 @@
-import Map from '../../components/map/Map';
-import Axios from "axios";
-import styles from '../../styles/dashboard.module.css';
-import Image from 'next/image';
-import Ethereum from '../../public/blockchains/ethereum.png';
-import TransactionsChart from '../../components/charts/TransactionsChart';
-import CumulativeTransactionsChart from '../../components/charts/CumulativeTransactionsChart';
+import React, { useState, useEffect, useRef } from "react";
+import Map from '../../components/map/Map'
+import Axios from "axios"
+import styles from '../../styles/dashboard.module.css'
+import Image from 'next/image'
+import Ethereum from '../../public/blockchains/ethereum.png'
+import TransactionsChart from '../../components/charts/TransactionsChart'
+import CumulativeTransactionsChart from '../../components/charts/CumulativeTransactionsChart'
+import { numberWithCommas, calculateSum, calculateMedian, emissionsData, cumEmissionsData, txData } from '../../data/emissionsData'
 
 const Dashboard = ({ mapData }) => {
+    const [footprint, setFootprint] = useState(null)
+    const [networkFootprint, setNetworkFootprint] = useState(null)
+    const [sumTransactions, setSumTransactions] = useState(null)
+    const [medianDailyTx, setMedianDailyTx] = useState(null)
+    // console.log('EMISSIONS DATA ', numberWithCommas(calculateSum(txData)));
+
+    useEffect(() => {
+        setFootprint(numberWithCommas(cumEmissionsData.at(-1)))
+        setNetworkFootprint(numberWithCommas(cumEmissionsData.at(-1), 2))
+        setSumTransactions(numberWithCommas(calculateSum(txData)))
+        setMedianDailyTx(numberWithCommas(calculateMedian(txData)))
+    });
 
     return (
         <div className={styles.dashboardContainer}>
@@ -22,7 +36,7 @@ const Dashboard = ({ mapData }) => {
             <p style={{"margin":"0"}}>0xEA123</p>
             <div className={styles.flexGrid}>
                 <div className={styles.grid}>
-                    <div className={styles.topDashboard}>2,000</div>
+                    <div className={styles.topDashboard}>{ footprint }</div>
                     <div className={styles.subHeader}>tCO&#8322;</div>
                     <p className={styles.subDashboard}>CO&#8322; Footprint</p>
                 </div>
@@ -42,7 +56,7 @@ const Dashboard = ({ mapData }) => {
                     <p className={styles.subHeaderTxt}>Network</p>
                     <ul className={styles.subEmissionsBar}>
                         <li>
-                            <div className={styles.subEmissionsTxt}>0</div>
+                            <div className={styles.subEmissionsTxt}>{ networkFootprint }</div>
                             <div className={styles.subHeader}>tCO&#8322;</div>
                             {/* <p className={styles.footnotes}>Group Validators</p> */}
                         </li>
@@ -74,7 +88,7 @@ const Dashboard = ({ mapData }) => {
                     </ul>
                     <ul className={styles.subStatsBar}>
                         <li>
-                            <div className={styles.subEmissionsTxt}>1,157,465.5</div>
+                            <div className={styles.subEmissionsTxt}>{ medianDailyTx }</div>
                             {/* <div className={styles.subHeader}>Tx</div> */}
                             <p className={styles.footnotes}>Median Daily Tx</p>
                         </li>
@@ -82,14 +96,14 @@ const Dashboard = ({ mapData }) => {
                     <hr className={styles.subHeaderDivider}></hr>
                     <ul className={styles.subStatsBar}>
                         <li>
-                            <div className={styles.subEmissionsTxt}>304.58</div>
+                            <div className={styles.subEmissionsTxt}>{ networkFootprint }</div>
                             <div className={styles.subHeader}>tCO&#8322;</div>
                             <p className={styles.footnotes}>YTD tCO&#8322;</p>
                         </li>
                     </ul>
                     <ul className={styles.subStatsBar}>
                         <li>
-                            <div className={styles.subEmissionsTxt}>30,457,818</div>
+                            <div className={styles.subEmissionsTxt}>{ sumTransactions }</div>
                             {/* <div className={styles.subHeader}>tCO&#8322;</div> */}
                             <p className={styles.footnotes}>YTD Tx</p>
                         </li>
