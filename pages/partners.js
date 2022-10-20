7094c4a52ee5b1563d7fde1da2d35de9f05d7f71
Mark Black <blackmarkt@gmail.com>
2022-10-20 10:14:40 -0700

    add partners blank page

---
+++ pages/partners.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Partners.module.css'
+
+export default function Parners() {
+    return (
+        <div className={styles.partnersMsgContainer}>
+                <div className={styles.partnersMsgSubContainer}>
+                    <h1 className={styles.partnersMsg}>Coming Soon</h1>
+                </div>
+        </div>
+        
+    )
+}
