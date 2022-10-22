ea36ac86f686b583fb3314f6e312c735758b75d2
Mark Black <blackmarkt@gmail.com>
2022-10-22 13:12:34 -0700

    fix Image import in Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-// import Image from 'next/Image'
+import Image from 'next/Image'
 import footerStyles from '../styles/Footer.module.css'
 import Twitter from '../public/social_media/twitter.png'
 
