c4151c078d1543bbcf9de4f7dc471021b8eea747
Mark Black <blackmarkt@gmail.com>
2022-10-23 08:01:08 -0700

    add last updated

--- pages/blockchains/index.jsx
+++ pages/blockchains/index.jsx
@@ -15,6 +15,7 @@ const Blockchains = () => {
         <div className={styles.dashboardContainer}>
             <div className={styles.dashboardLeftHeader}>
                 <h2 className={styles.dashboardMainHeader}>Blockchain (L1) Emissions</h2>
+                <p style={{"margin":"0 0 1rem", "padding": "0rem"}}>Last Updated 11/22/22</p>
             </div>
             <div className={styles.dashboardRightHeader}>
                 <a href="https://www.notion.so/blackmarkt/Carbon-Accounting-Methodology-b2e03be009a945f680592ec7563107d6" target="_blank" rel="noopener noreferrer">
