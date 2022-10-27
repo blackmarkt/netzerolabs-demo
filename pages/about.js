10183ac65d071e9bc6519c053d723d844e81aead
Mark Black <blackmarkt@gmail.com>
2022-10-27 14:29:21 -0700

    minor changes to text

--- pages/about.js
+++ pages/about.js
@@ -4,13 +4,15 @@ export default function About() {
     return (
         <div className={aboutStyles.aboutContainer}>
                 <div className={aboutStyles.aboutSubContainer}>
-                    <h2 className={aboutStyles.aboutHeader}>Our Web3 Mission</h2>
+                    <h2 className={aboutStyles.aboutHeader}>Our Web3 Climate Mission</h2>
                     <p>All Web3 companies, organizations, DAO's, etc... that have publicly launched tokens commit to the following 2 climate actions:</p>
                     <div className={aboutStyles.aboutSubContainer}>
-                        <h3 className={aboutStyles.aboutMissionState}>NetZero 20X0 Pledges</h3>
-                        <p>Draft accessible, credible, actionable and enforceable plans to become NetZero by 2050 or sooner. Ideally these plans would be tokenized and stored on-chain </p>
+                        <h3 className={aboutStyles.aboutMissionState}>NetZero 20x0 Pledges</h3>
+                        <p className={aboutStyles.aboutTxt}>Draft accessible, credible, actionable and enforceable plans to become NetZero by 2050 or sooner (2030 being the most ambitious deadline). Ideally these plans would be tokenized and stored on-chain. To learn more please check out the  
+                            <a target="_blank" href="https://www.un.org/en/climatechange/net-zero-coalition" style={{'text-decoration': "underline", "color": "aquamarine"}}> UN NetZero Coalition</a>
+                        </p>
                         <h3 className={aboutStyles.aboutMissionState}>Carbon Neutral Commitment</h3>
-                        <p>Consistently measure, report and offset carbon footprints. Ideally all reports, assets, certifications would be tokenized and stored on-chain</p>
+                        <p className={aboutStyles.aboutTxt}>Consistently measure, report and offset carbon footprints. Ideally all reports, assets, certifications would be tokenized and stored on-chain</p>
                     </div>
                 </div>
         </div>
