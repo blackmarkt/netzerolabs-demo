42b04b1d6e977a6b93da3ca7acf8d237d1af687e
Mark Black <blackmarkt@gmail.com>
2022-11-17 14:46:53 -0800

    change About to Proof of NetZero

--- pages/about.js
+++ pages/about.js
@@ -13,9 +13,9 @@ export default function About() {
                 <h1 className={aboutStyles.aboutHeader}>Our Climate Mission</h1>
                 <p className={aboutStyles.aboutHeaderTxt}>All Web3 companies, organizations, DAO's, etc... that have publicly launched tokens commit to the following 2 climate actions:</p>
                 <div className={aboutStyles.aboutSubCommitContainer}>
-                    <h2 className={aboutStyles.aboutMissionState}>Carbon Negative Commitment</h2>
+                    <h2 className={aboutStyles.aboutMissionState}>Proof of NetZero</h2>
                     <p className={aboutStyles.aboutTxt}>Consistently measure, report and offset carbon footprints. All climate reports, assets, certifications should be tokenized and stored on-chain for absolute public transparency.</p>
-                    <h2 className={aboutStyles.aboutMissionState}>NetZero 20x0 Pledge</h2>
+                    <h2 className={aboutStyles.aboutMissionState}>NetZero 20X0 Pledge</h2>
                     <p className={aboutStyles.aboutTxt}>Draft accessible, credible, actionable and enforceable plans to become NetZero by 2050 or sooner (2030 being the most ambitious deadline). Ideally these plans would be tokenized and stored on-chain. To learn more please check out the  
                         <a target="_blank" href="https://www.un.org/en/climatechange/net-zero-coalition" style={{'text-decoration': "underline", "color": "aquamarine"}}> UN NetZero Coalition</a>
                     </p>
